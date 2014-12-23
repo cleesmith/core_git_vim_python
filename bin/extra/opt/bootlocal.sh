9e6cae1d375c3c0e4be279ac530c0932f66e9ccd
Chris Smith <cleesmith2006@gmail.com>
2014-12-23 02:30:04 -0500

    update readme

--- bin/extra/opt/bootlocal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# put other system startup commands here
-chown tc:staff /home/tc
-mkdir -p /home/tc/.local
-chown tc:staff /home/tc/.local
-sudo ln -sf /usr/local/bin/python2.7 /usr/bin/python
-sudo ln -sf /usr/local/bin/python2.7 /usr/local/bin/python
-sudo ln -sf /usr/local/bin/easy_install /usr/bin/easy_install
-
